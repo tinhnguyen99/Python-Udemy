b41c59b866f4b858fa994ce8488da1d4ab34a96b
tinhnguyen99 <tinh.nguyenhayho@hcmut.edu.vn>
2022-02-16 01:26:31 +0700

    Change virtual env
    Singed-off-by: Tinh Nguyen <tinh.nguyentrung1999@gmail.com>

---
+++ day16/tinhnturtle.py
@@ -0,0 +1,14 @@
+# Tinh Nguyen <tinh.nguyentrung1999@gmail.com>
+from turtle import Turtle, Screen
+from prettytable import table
+
+
+Timmy = Turtle()
+print(Timmy)
+print("hello world")
+Timmy.shape("classic")
+
+
+MyScreen = Screen()
+print(MyScreen)
+MyScreen.exitonclick()
