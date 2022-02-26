0c13dcad62e772f07a341f2a5f699065e7fca682
tinhnguyen99 <tinh.nguyenhayho@hcmut.edu.vn>
2022-02-27 00:30:43 +0700

    Day18: Turtle
    Challenge 1
    
    Signed-off-by: Tinh Nguyen <tinh.nguyentrung1999@gmail.com>

---
+++ day18/challenge1.py
@@ -0,0 +1,22 @@
+# Tinh Nguyen <tinh.nguyentrung1999@gmail.com>
+
+#Draw a square
+
+from turtle import Turtle, Screen
+
+
+timmy = Turtle()
+timmy_s = Screen()
+
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+
+#done
+
+
+timmy_s.exitonclick()
